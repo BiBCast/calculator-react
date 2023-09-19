c8d743ba63c7e94534aae038283fe095d5da5198
BiBCast <pietrocast03@gmail.com>
2023-09-19 19:56:47 +0200

    hover created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Display } from "./components/Display";
 import { KeyPad } from "./components/Keypad";
 import { Navigation } from "./components/Navigation";
 import "./index.css";
 function App() {
+  const [selectedOption, setSelectedOptions] = useState<number>(2);
+  {
+    console.log(selectedOption);
+  }
   return (
     <div className="container">
       <div className="container__cellphone">
-        <Navigation options={["mario", "gigi"]} />
+        <Navigation
+          options={["mario", "gigi", "pè"]}
+          selectedIndex={selectedOption}
+          onclick={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
         <Display />
         <KeyPad />
       </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,5 @@
+import { CSSProperties, useState } from "react";
+
 type Props = {
   options: string[];
   selectedIndex: number;
@@ -5,12 +7,29 @@ type Props = {
 };
 
 export function Navigation({ options, selectedIndex, onclick }: Props) {
+  const styleOptions = { "--options": options.length } as CSSProperties;
+  {
+    console.log(styleOptions);
+  }
   return (
     <>
       <div className="cellphone__navigation">
-        {options.map((opt) => {
-          return <div className="navigation__element"> {opt}</div>;
+        {options.map((opt, index) => {
+          return (
+            <div key={index} className="navigation__element">
+              {opt}
+            </div>
+          );
         })}
+        <div
+          className="navigation__hover"
+          style={
+            {
+              ...styleOptions,
+              "--selected-option": selectedIndex,
+            } as CSSProperties
+          }
+        ></div>
       </div>
     </>
   );
