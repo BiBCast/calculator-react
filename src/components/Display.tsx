d3173801ea429df2268f30776bcd91384f9e4996
BiBCast <pietrocast03@gmail.com>
2023-09-21 20:22:46 +0200

    layout keypad

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,7 +1,9 @@
 export function Display() {
   return (
     <>
-      <div className="cellphone__display"></div>
+      <div className="cellphone__display">
+        <span className="display__text">prova</span>
+      </div>
     </>
   );
 }
--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -18,10 +18,12 @@ export function KeyPad() {
         <div className="keypad__button"></div>
         <div className="keypad__button"></div>
         <div className="keypad__button"></div>
-        <div className="keypad__button"></div>
+        <div className="keypad__button button_large"></div>
         <div className="keypad__button"></div>
         <div className="keypad__button"></div>
       </div>
+
+      <div style={{ height: "5rem" }}></div>
     </>
   );
 }
